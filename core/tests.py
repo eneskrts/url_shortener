b7ef42392cd9df02a676dc00a3dc2df13f22d91c
enes.karatas <eneskrts.dev@gmail.com>
2025-05-07 14:13:31 +0300

    view test cases created

--- core/tests.py
+++ core/tests.py
@@ -5,8 +5,27 @@ from core.models import ShortenedUrl
 
 
 class RedirectUrlTestCase(TestCase):
-    pass
-    # TODO: url shortening,
-    # TODO: redirect works,
-    # TODO: redirect click counter,
-    # TODO: invalid short url
+    def setUp(self):
+        self.client = Client()
+        self.test_url = "https://enes.com"
+        self.shortened = ShortenedUrl.objects.create(original_url=self.test_url)
+
+    def test_url_shortening(self):
+        response = self.client.post(reverse('short_url_create'), {'original_url': 'https://enes.com'})
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(ShortenedUrl.objects.filter(original_url='https://enes.com').exists())
+
+    def test_redirect_works(self):
+        response = self.client.get(reverse('redirect_short_url', kwargs={'code': self.shortened.shortened_url}))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, self.test_url)
+
+    def test_redirect_click_counter(self):
+        initial_count = self.shortened.click_count
+        self.client.get(reverse('redirect_short_url', kwargs={'code': self.shortened.shortened_url}))
+        self.shortened.refresh_from_db()
+        self.assertEqual(self.shortened.click_count, initial_count + 1)
+
+    def test_invalid_short_url(self):
+        response = self.client.get(reverse('redirect_short_url', kwargs={'code': 'nonexistent'}))
+        self.assertEqual(response.status_code, 404)
